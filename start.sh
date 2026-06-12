156d222058936ea9df274efb60e3be64b15f6b98
sruparagon-tech <sruparagon@gmail.com>
2026-06-12 12:00:43 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,6 +9,13 @@ mysql -e "FLUSH PRIVILEGES;"
 
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '';" 2>/dev/null || true
 
+# Copy config file jika belum ada
+cd /var/www/html/DVWA
+if [ ! -f config/config.inc.php ]; then
+    cp config/config.inc.php.dist config/config.inc.php
+    sed -i "s/'db_password' => ''/'db_password' => 'p@ssw0rd'/" config/config.inc.php
+fi
+
 # Ganti port Apache dengan $PORT dari Railway
 sed -i "s/Listen 80/Listen ${PORT:-80}/g" /etc/apache2/ports.conf
 sed -i "s/:80/:${PORT:-80}/g" /etc/apache2/sites-available/000-default.conf
