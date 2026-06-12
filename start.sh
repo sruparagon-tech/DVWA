c8944dee03c32a25a18baedaa76ebb76b3d5aca7
sruparagon-tech <sruparagon@gmail.com>
2026-06-12 11:38:17 +0700

    Change Apache port to use $PORT variable
    Update Apache configuration to use dynamic port from environment variable.

--- start.sh
+++ start.sh
@@ -9,5 +9,9 @@ mysql -e "FLUSH PRIVILEGES;"
 
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '';" 2>/dev/null || true
 
+# Ganti port Apache dengan $PORT dari Railway
+sed -i "s/Listen 80/Listen ${PORT:-80}/g" /etc/apache2/ports.conf
+sed -i "s/:80/:${PORT:-80}/g" /etc/apache2/sites-available/000-default.conf
+
 source /etc/apache2/envvars
 exec apache2 -D FOREGROUND
