9dfc915f49c9c3c1999e8dfe9df9a5ece17e917b
sruparagon-tech <sruparagon@gmail.com>
2026-06-12 10:51:18 +0700

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,14 @@
+bash
+#!/bin/bash
+service mariadb start
+sleep 2
+
+mysql -e "CREATE DATABASE IF NOT EXISTS dvwa;"
+mysql -e "CREATE USER IF NOT EXISTS 'dvwa'@'localhost' IDENTIFIED BY 'p@ssw0rd';"
+mysql -e "GRANT ALL ON dvwa.* TO 'dvwa'@'localhost';"
+mysql -e "FLUSH PRIVILEGES;"
+
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '';" 2>/dev/null || true
+
+source /etc/apache2/envvars
+exec apache2 -D FOREGROUND
